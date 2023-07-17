741b244224f22de507bdf8136358505e4e348931
ChristopheThiou <christophe.thiou.simplon@gmail.com>
2023-07-17 10:53:29 +0200

    first commit

--- src/Controller/NoteController.php
+++ src/Controller/NoteController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Student;
 use App\Repository\NoteRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/api/note')]
 class NoteController extends AbstractController
@@ -20,5 +22,21 @@ class NoteController extends AbstractController
     public function all(): JsonResponse
     {
         return $this->json($this->repo->findAll());
+    }
+    #[Route('/{id}', methods: 'POST')]
+    public function add(Request $request, SerializerInterface $serializer, Student $student): JsonResponse {
+        try {
+            $note = $serializer->deserialize($request->getContent(), Note::class, 'json');
+        } catch (\Exception $e) {
+            return $this->json('Invalid Body', 400);
+        }
+
+        $note->setStudent($student);
+        $note->setCreatedAt(new \DateTime());
+        
+        $this->em->persist($note);
+        $this->em->flush();
+        return $this->json($note, 201);
+
     }
 }
